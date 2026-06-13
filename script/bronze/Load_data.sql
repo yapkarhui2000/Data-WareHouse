83a05a7aa981d309da014a0e2216302ec37fcb5f
kar hui yap <yapkarhui2000@gmail.com>
2026-06-13 15:36:29 +0100

    Add files via upload

---
+++ script/bronze/Load_data.sql
@@ -0,0 +1,140 @@
+/*
+Title: Truncating and Loading data
+*/
+
+/* create procedure so that it is easy to load and truncating for new data*/
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME,@end_time DATETIME,@batch_start_time DATETIME,@batch_end_time DATETIME;
+	BEGIN TRY
+	SET @batch_start_time = GETDATE();
+	PRINT '========================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '========================================';
+
+	PRINT '------------------------------------------';
+	PRINT 'Loading CRM tables'
+	PRINT '------------------------------------------';
+
+	SET @start_time =GETDATE();
+
+	PRINT '>> Truncating table: bronze.crm_cust_info ';
+	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Inserting table: bronze.crm_cust_info ';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds';  
+	PRINT '----------------';
+
+	SET @start_time =GETDATE();
+
+	PRINT '>> Truncating table: bronze.crm_prd_info ';
+	TRUNCATE TABLE bronze.crm_prd_info;
+	PRINT '>> Inserting table: bronze.crm_prd_info ';
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds';  
+	PRINT '----------------';
+
+	SET @start_time =GETDATE();
+
+	PRINT '>> Truncating table: bronze.crm_sales_details ';
+	TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>> Inserting table: bronze.crm_sales_details ';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds';  
+	PRINT '------------------------------------------';
+	PRINT 'Loading CRM tables'
+	PRINT '------------------------------------------';
+
+
+	PRINT '------------------------------------------';
+	PRINT 'Loading ERP tables'
+	PRINT '------------------------------------------';
+	
+	SET @start_time =GETDATE();
+
+	PRINT '>> Truncating table: bronze.erp_cust_az12 ';
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	PRINT '>> Inserting table: bronze.erp_cust_az12 ';
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds'; 
+	PRINT '----------------';
+
+	SET @start_time =GETDATE();
+	PRINT '>> Truncating table: bronze.erp_loc_a101 ';
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	PRINT '>> Inserting table: bronze.erp_loc_a101 ';
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds'; 
+	PRINT '----------------';
+
+	SET @start_time =GETDATE();
+	PRINT '>> Truncating table: bronze.erp_px_cat_g1v2 ';
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	PRINT '>> Inserting table: bronze.erp_px_cat_g1v2 ';
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\karhu\OneDrive\Desktop\DataWareHouse Project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW =2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time =GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds'; 
+	PRINT '------------------------------------------';
+	PRINT 'Loading CRM tables'
+	PRINT '------------------------------------------';
+
+	SET @batch_end_time =GETDATE();
+	PRINT'===================================';
+	PRINT'Loading Bronze Layer is Completed'
+	PRINT' -TOTAL LOAD DUARATION:' + CAST(DATEDIFF(second,@batch_start_time,@batch_end_time) AS NVARCHAR) + 'seconds';
+	PRINT'===================================';
+	END TRY
+	BEGIN CATCH
+		PRINT '=======================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' +CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '=======================';
+	END CATCH
+
+END
